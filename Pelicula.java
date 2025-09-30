2c855899961f10e32cd57c48fe44735fd5c60893
RaulAG-Dev <a24216890@alumnos.uady.mx>
2025-09-30 06:00:30 +0000

    Implementación de atributos privados, setters y getters de la clase Pelicula

--- Pelicula.java
+++ Pelicula.java
@@ -1,25 +1,23 @@
+public class Pelicula{
 
+    //Atributos 
+    private int id;
+    private int año;
+    private float calificacion;
+    private String nombre;
+    private String notas;
 
+    //Constructores
+    public Pelicula(){
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public Pelicula(int id, String nombre, int año, float calificacion, String notas){
+        this.id = id;
+        this.nombre = nombre;
+        this.año = año;
+        this.calificacion = calificacion;
+        this.notas = notas;
+    }
 
 }
